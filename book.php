8e181c80c6dc1f1f6216ad2c7baf3d72bc9938a0
Razexay <razexay@gmail.com>
2020-09-22 15:38:36 +0300

    done

--- book.php
+++ book.php
@@ -14,7 +14,8 @@ $book = Book::findById($id);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index_style.css">
+    <link rel="stylesheet" href="book_style.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand" />
     <title>Document</title>
 </head>
 
--- index.php
+++ index.php
@@ -13,17 +13,21 @@ $books = Book::findAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index_style.css">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand" />
     <title>Bookstore</title>
 </head>
 
 <body>
 
     <body>
-        <ul>
-            <?php foreach ($books as $book) { ?>
-                <li><a href="book.php?id=<?php echo $book->id; ?>"><?php echo $book->title; ?></a></li>
-            <?php } ?>
-        </ul>
+        <div>
+            <ul>
+                <h1>Bookstore</h1>
+                <?php foreach ($books as $book) { ?>
+                    <li><a href="book.php?id=<?php echo $book->id; ?>"><?php echo $book->title; ?></a></li>
+                <?php } ?>
+            </ul>
+        </div>
     </body>
 </body>
 
